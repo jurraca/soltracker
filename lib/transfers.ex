aa03002565feedd41c88c2a1ed1e7a677589f94b
julienu <julienu@pm.me>
2021-12-03 00:16:23 +0100

    add Rustler, rust lib, and metadata decoding from a program-derived address

--- lib/transfers.ex
+++ lib/transfers.ex
@@ -1,5 +1,9 @@
 defmodule SolTracker.Transfers do
 
+	use Rustler,
+		otp_app: :soltracker,
+		crate: :metaplex_decoder 
+
 	# The Solana SystemProgram, a str len 32
 	@system_program "11111111111111111111111111111111"
 	@token_program "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
@@ -24,4 +28,21 @@ defmodule SolTracker.Transfers do
 	end
 
 	def decode(msg), do: msg
+
+	def metadata_from_pda(pda) do 
+		pda
+		|> pda_to_b58()
+		|> deserialize_metadata()
+		|> Jason.decode()
+	end
+
+	def pda_to_b58(pda) do
+		{:ok, bin} = B58.decode58(pda)
+		request = Solana.RPC.Request.get_account_info(bin, %{"encoding" => "base64"})
+	   	{:ok, %{"data" => data}} = Solana.RPC.send(SolTracker.rpc_client(), request)
+	   	{:ok, b} = Base.decode64(Enum.at(data, 0))
+	   	B58.encode58(b)
+	end
+
+	def deserialize_metadata(arg), do: :erlang.nif_error(:nif_not_loaded)
 end
--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule SolTracker.MixProject do
   defp deps do
     [
       {:solana, "~> 0.1.3"},
-      {:websockex, "~> 0.4.3"}
+      {:websockex, "~> 0.4.3"},
+      {:rustler, "~> 0.22.2"}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
